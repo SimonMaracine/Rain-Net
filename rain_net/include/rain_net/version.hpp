6536db014e0710faaae3f9a83c3eb5972e00dd68
SimonMaracine <simonmaracine@gmail.com>
2023-09-11 19:22:11 +0300

    Increment version

--- rain_net/include/rain_net/version.hpp
+++ rain_net/include/rain_net/version.hpp
@@ -2,6 +2,6 @@
 
 namespace rain_net {
     inline constexpr unsigned int VERSION_MAJOR = 0;
-    inline constexpr unsigned int VERSION_MINOR = 5;
-    inline constexpr unsigned int VERSION_PATCH = 1;
+    inline constexpr unsigned int VERSION_MINOR = 6;
+    inline constexpr unsigned int VERSION_PATCH = 0;
 }
